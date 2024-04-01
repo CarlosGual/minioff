3b03fa3fb4cf5495eef7efb9ec2a3b2874be9bcc
carlos <carlos@gumior.es>
2024-04-01 14:44:08 +0900

    working example

---
+++ test.py
@@ -0,0 +1,32 @@
+from ray.rllib.algorithms import PPOConfig
+from ray.tune.registry import register_env
+
+
+def env_creator(env_config):
+    import gymnasium as gym
+    import miniworld
+    env = gym.make("MiniWorld-Maze-v0")
+    resized_env = gym.wrappers.ResizeObservation(env, (84, 84))
+    return resized_env
+
+
+register_env("MiniWorld-Maze-v0", env_creator)
+
+config = (
+    PPOConfig()
+    .environment("MiniWorld-Maze-v0")
+    .rollouts(num_rollout_workers=2)
+    .framework("torch")
+    .training(model={
+        "dim": 84,
+        "fcnet_hiddens": [64, 64],
+    })
+    .evaluation(evaluation_num_workers=1)
+)
+
+algo = config.build()
+
+for _ in range(5):
+    print(algo.train())
+
+algo.evaluate()
